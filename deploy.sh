a1fac68efffaeffe90ef9cd8d016431ed714fb62
Hans Dushanthakumar <hansrd_98@yahoo.com>
2016-10-13 17:53:30 +1100

    Parameterised rancher-compose and docker-compose files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-usage="$(basename "$0") [-h] [-e ENVIRONMENT] [-s STACK] [-c SERVICE] [-r RANCHER_COMMAND] [-w WAIT_TIME_SECS] -- script to upgrade and deploy containers in the given environment.
+usage="$(basename "$0") [-h] [-e ENVIRONMENT] [-s STACK] [-c SERVICE] [-r RANCHER_COMMAND] [-d DOCKER_COMPOSE_FILE] [-n RANCHER_COMPOSE_FILE] [-w WAIT_TIME_SECS] -- script to upgrade and deploy containers in the given environment.
 Make sure that you have rancher environment options set and rancher cli installed before running the script.
 
 where:
@@ -9,16 +9,20 @@ where:
 -s  set the rancher stack (default: QA)
 -c  set the rancher service (default: poseidon-app)
 -r  set the rancher command (default: ./rancher)
+-d  set the docker-compose file (default: deployment/docker-compose.yml)
+-n  set the rancher-compose file (default: deployment/rancher-compose.yml)
 -w  set the wait time in seconds (default: 120)"
 
 env=Dev
 stack=QA
 service=poseidon-app
 rancher_command=./rancher
+docker_compose_file=deployment/docker-compose.yml
+rancher_compose_file=deployment/rancher-compose.yml
 WAIT_TIMEOUT=120
 NUMBER_OF_TIMES_TO_LOOP=$(( $WAIT_TIMEOUT/10 ))
 
-while getopts ':e:s:c:r:w:h' option; do
+while getopts ':e:s:c:r:w:d:n:h' option; do
     case "$option" in
         h)  echo "$usage"
             exit
@@ -30,6 +34,10 @@ while getopts ':e:s:c:r:w:h' option; do
         c)  service=$OPTARG
             ;;
         r)  rancher_command=$OPTARG
+            ;;
+        d)  docker_compose_file=$OPTARG
+            ;;
+        n)  rancher_compose_file=$OPTARG
             ;;
         w)  WAIT_TIMEOUT=$OPTARG
             NUMBER_OF_TIMES_TO_LOOP=$(( $WAIT_TIMEOUT/10 ))
@@ -48,7 +56,7 @@ shift $((OPTIND - 1))
 
 function upgrade_the_service(){
     echo  "Upgrading $service in $env"
-    $rancher_command --environment $env --debug --wait --wait-timeout $WAIT_TIMEOUT up --batch-size 1 -s $stack -f deployment/docker-compose.yml --rancher-file deployment/rancher-compose.yml --upgrade -p -d
+    $rancher_command --environment $env --debug --wait --wait-timeout $WAIT_TIMEOUT up --batch-size 1 -s $stack -f $docker_compose_file --rancher-file $rancher_compose_file --upgrade -p -d
     monitor_transition
     state_after_upgrade=`$rancher_command --environment $env inspect --format '{{ .state}}' $stack/$service | head -n1`
     echo  "The state of service after upgrade is $state_after_upgrade"
@@ -68,7 +76,7 @@ function finish_upgrade(){
     if [[ $health_status == "healthy" ]]
         then
             echo "Upgraded service successfully. Confirming Upgrade."
-            $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up -s $stack -f deployment/docker-compose.yml --rancher-file deployment/rancher-compose.yml --confirm-upgrade -d
+            $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up -s $stack -f $docker_compose_file --rancher-file $rancher_compose_file --confirm-upgrade -d
             echo "Upgraded service successfully and confirmed."
             wait_for_service_to_have_status "healthy" ".healthState"
             exit 0
@@ -79,7 +87,7 @@ function finish_upgrade(){
 
 function roll_back(){
     echo "Upgrade failed. Initiating rollback to the previous deployed version"
-    $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up --batch-size 1 -s $stack -f deployment/docker-compose.yml --rancher-file deployment/rancher-compose.yml --rollback -d
+    $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up --batch-size 1 -s $stack -f $docker_compose_file --rancher-file $rancher_compose_file --rollback -d
     wait_for_service_to_have_status "healthy" ".healthState"
     echo "Upgrade failed. Rolled back to the previous deployed version"
     exit 1
@@ -101,7 +109,7 @@ function confirm_upgrade_if_previous_upgrade_pending() {
     if [[ "$service_status" == "upgraded" ]]
     then
         echo "Previous Upgrade not completed. Confirming the previous Upgrade before continuing."
-        $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up -s $stack -f deployment/docker-compose.yml --rancher-file deployment/rancher-compose.yml --confirm-upgrade -d
+        $rancher_command --environment $env --debug --wait --wait-state active --wait-timeout $WAIT_TIMEOUT up -s $stack -f $docker_compose_file --rancher-file $rancher_compose_file --confirm-upgrade -d
         wait_for_service_to_have_status "healthy" ".healthState"
     fi
 }
