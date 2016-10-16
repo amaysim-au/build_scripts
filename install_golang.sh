efd1d02815a83479ef4284397e9f15681f64cd4e
Yun Zhi Lin <yun@yunspace.com>
2016-10-16 14:04:49 +1100

    fixed argument typo

--- install_golang.sh
+++ install_golang.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -e
 
-GOVERSION=$0
+GOVERSION=$1
 
 # install go
+echo "installing golang ${GOVERSION}"
 cd /usr/local && wget --quiet https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz -O - | sudo tar -xz
+mkdir $HOME/go
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 # install glide
+echo "installing glide latest"
 sudo apt-get -y --no-install-recommends -qq install software-properties-common python-software-properties
 sudo add-apt-repository ppa:masterminds/glide && sudo apt-get -qq update
 sudo apt-get -y install golang glide
