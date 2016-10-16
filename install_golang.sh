9c1a217bb25131126c1aea011b41b6383744e48f
Yun Zhi Lin <yun@yunspace.com>
2016-10-16 18:07:41 +1100

    merged glide install script

--- install_golang.sh
+++ install_golang.sh
@@ -5,12 +5,17 @@ WORKDIR=$PWD
 
 # install go
 echo "installing golang ${GOVERSION}"
-cd /usr/local 
-wget --quiet https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz -O - | sudo tar -xz
-cd $WORKDIR
-
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
+cd /usr/local
 if [ ! -d $GOPATH ]; then
 	mkdir $GOPATH
 fi
+
+wget --quiet https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz -O - | sudo tar -xz
+cd $WORKDIR
+
+PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+go version
+
+# install glide
+curl https://glide.sh/get | sh
+glide version
