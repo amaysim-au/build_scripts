a3398b2853579064fc60b3b50afffe11dd026a06
Yun Zhi Lin <yun@yunspace.com>
2016-10-16 17:31:38 +1100

    GOPATH refactor

--- install_golang.sh
+++ install_golang.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
 GOVERSION=$1
+WORKDIR=$PWD
 
 # install go
 echo "installing golang ${GOVERSION}"
-cd /usr/local && wget --quiet https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz -O - | sudo tar -xz
-mkdir $HOME/go
-export GOPATH=$HOME/go
-export GOROOT=/usr/local/go
+cd /usr/local 
+wget --quiet https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz -O - | sudo tar -xz
+cd $WORKDIR
+
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+if [ ! -d $GOPATH ]; then
+	mkdir $GOPATH
+fi
