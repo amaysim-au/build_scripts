d5d39993033fc11819ccfc1f31af9604ecf367d8
Yun Zhi Lin <yun@yunspace.com>
2016-10-17 16:03:58 +1100

    VUL-94 quiet wget for glide

--- install_golang.sh
+++ install_golang.sh
@@ -35,9 +35,8 @@ function installGlide() {
 	local glideVersion="v0.12.3"
 	echo "installing glide ${glideVersion}"
 
-	wget https://github.com/Masterminds/glide/releases/download/${glideVersion}/glide-${glideVersion}-linux-amd64.tar.gz -O - | tar -xz
+	wget --quiet https://github.com/Masterminds/glide/releases/download/${glideVersion}/glide-${glideVersion}-linux-amd64.tar.gz -O - | tar -xz
 	sudo mv linux-amd64/glide /usr/local/bin/
-	# curl -s https://glide.sh/get | sh
 }
 
 if [ "$#" -ne 3 ]; then
@@ -45,8 +44,8 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-#installGolang $1
-#symlinkGoPath $2 $3
-#PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+installGolang $1
+symlinkGoPath $2 $3
+PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 installGlide
