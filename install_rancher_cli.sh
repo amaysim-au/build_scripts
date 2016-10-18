3322a17b02290ee6b5daac89fce6c859e4a5c9e4
Hans Dushanthakumar <hansrd_98@yahoo.com>
2016-10-18 16:08:13 +1100

    Fixed install rancher script

--- install_rancher_cli.sh
+++ install_rancher_cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-wget --quiet https://github.com/rancher/cli/releases/download/v0.2.0/rancher-linux-amd64-v0.2.0.tar.gz -O | tar xz
+wget -qO- https://github.com/rancher/cli/releases/download/v0.2.0/rancher-linux-amd64-v0.2.0.tar.gz | tar xz
 sudo mv ./rancher-v0.2.0/rancher /usr/local/bin/rancher
 rancher -version
