499d2a8cf5e1e89a6eb24c46f9c860a363bb0317
Kale Worsley <kale@worsley.co.nz>
2016-11-01 13:08:26 +1300

    Don't append to the list file.

--- install_postgres.sh
+++ install_postgres.sh
@@ -6,7 +6,7 @@ PSQL_PORT=${PSQL_PORT:-5433}
 
 function addPostgresqlRepo() {
   echo "adding official postgresql repo"
-  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release  -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release  -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
   sudo apt-get update
 }
